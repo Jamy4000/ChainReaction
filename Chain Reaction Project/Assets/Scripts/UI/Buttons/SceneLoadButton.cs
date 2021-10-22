b9bd9c585fdefe5c9067d51f3c8cecdc7e1dd5b7
lolesch <leonid.schreiber@yahoo.de>
2021-10-22 23:20:10 +0200

    [+] added a base class for UI buttons - the mainMenu can now load the ingame scene

---
+++ Chain Reaction Project/Assets/Scripts/UI/Buttons/SceneLoadButton.cs
@@ -0,0 +1,14 @@
+﻿using ChainReaction.AttributeRef;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace ChainReaction.UI
+{
+    public class SceneLoadButton : AddListenerButton
+    {
+        [SceneRef, SerializeField]
+        private string sceneToLoad;
+
+        protected override void OnClick() => SceneManager.LoadScene(sceneToLoad, LoadSceneMode.Single);
+    }
+}
