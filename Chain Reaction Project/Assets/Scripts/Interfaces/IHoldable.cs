c568565dc4029fd3e3d75ee4ce8678d13ee0b56f
teppich <johannesspicker@me.com>
2021-10-22 23:18:24 +0200

    Added IHoldable

---
+++ Chain Reaction Project/Assets/Scripts/Interfaces/IHoldable.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace Interfaces
+{
+    public interface IHoldable
+    {
+        /// <summary>
+        ///     Called when picking up the IHoldable.
+        ///     AI will reparent the transform onto itself.
+        /// </summary>
+        /// <returns>The parent transform of the IHoldable</returns>
+        Transform PickUp();
+
+        /// <summary>
+        ///     Called when putting down the IHoldable.
+        ///     AI will deparent transform from itself.
+        /// </summary>
+        /// <param name="position"></param>
+        /// <returns>Whether the position was valid.</returns>
+        bool PutDown(Vector3 position);
+    }
+}
