28801c25c577eccbf7b060141737ae4f1d780f6c
N-Banchik <87769833+N-Banchik@users.noreply.github.com>
2021-10-23 00:20:37 +0300

    Added Player preferb,added Player movement script.

---
+++ Chain Reaction Project/Assets/Scripts/Movement/PlayerMovement.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]
+    [Tooltip("Change the Movement speed of the player")]
+    [Range(0f,10f)]
+    private float MovemntSpeed;
+    [SerializeField]
+    [Tooltip("Change the turning speed of the player,the higher the number the faster the trurning")]
+    [Range(0f, 10f)]
+    private float TurningSpeed;
+    [SerializeField]
+    [Tooltip("Pick the rigid body of the object")]
+    private Rigidbody _Rigidbody;
+
+    private float X;
+    private float Z;
+    // Start is called before the first frame update
+    private void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        X = Input.GetAxis("Horizontal") * MovemntSpeed;
+        Z = Input.GetAxis("Vertical") * MovemntSpeed;
+
+        _Rigidbody.velocity = new Vector3(X, _Rigidbody.velocity.y, Z);
+
+        Vector3 vel = _Rigidbody.velocity;
+        vel.y = 0.0f;
+        if (vel.x != 0 || vel.z != 0)
+        {
+            _Rigidbody.transform.forward = Vector3.Lerp(_Rigidbody.transform.forward, vel, TurningSpeed * Time.deltaTime);
+        }
+    }
+}
