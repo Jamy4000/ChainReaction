cb437cfe6787e7cdc015837daac5ba3c89b873e0
Arie <arieworkmail@gmail.com>
2021-10-23 17:59:23 +0300

    Fix for the player's falling speed

--- Chain Reaction Project/Assets/Scripts/Movement/PlayerMovement.cs
+++ Chain Reaction Project/Assets/Scripts/Movement/PlayerMovement.cs
@@ -38,7 +38,7 @@ public class PlayerMovement : MonoBehaviour
         X = Input.GetAxis("Horizontal") * MovemntSpeed;
         Z = Input.GetAxis("Vertical") * MovemntSpeed;
 
-        _Rigidbody.velocity = new Vector3(X,0, Z);
+        _Rigidbody.velocity = new Vector3(X, _Rigidbody.velocity.y, Z);
 
         Vector3 vel = _Rigidbody.velocity;
         //vel.y = 0.0f;
