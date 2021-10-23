160fcf1b3eae7f3561706184de3810d7ff29a05d
Arie <arieworkmail@gmail.com>
2021-10-23 19:35:59 +0300

    Added holdable events, added BombRandomizer class for adding bombs to conveyor belts.

--- Chain Reaction Project/Assets/Scripts/Holdables/Holdable.cs
+++ Chain Reaction Project/Assets/Scripts/Holdables/Holdable.cs
@@ -2,8 +2,32 @@
 
 namespace Holdables
 {
+    public delegate void HoldObject(Holdable holdable);
+    public delegate void DropObject(Holdable holdable);
+
     public class Holdable : MonoBehaviour
     {
-        
+        public event HoldObject Held;
+        public event DropObject Dropped;
+
+        public void HoldObject()
+        {
+            OnObjectHold();
+        }
+
+        protected virtual void OnObjectHold()
+        {
+            Held?.Invoke(this);
+        }
+
+        public void DropObject()
+        {
+            OnObjectDropped();
+        }
+
+        protected virtual void OnObjectDropped()
+        {
+            Dropped?.Invoke(this);
+        }
     }
 }
