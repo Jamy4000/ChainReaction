84650acefd5949627a3e8f5a0c553a651b09d8e5
lolesch <leonid.schreiber@yahoo.de>
2021-10-23 19:26:13 +0200

    Added 3 explosives prefab variants with their models

--- Chain Reaction Project/Assets/Scripts/Interactions/ExplosionForce.cs
+++ Chain Reaction Project/Assets/Scripts/Interactions/ExplosionForce.cs
@@ -1,11 +1,15 @@
 ﻿using System;
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace ChainReaction
 {
     public class ExplosionForce : MonoBehaviour
     {
+        [SerializeField] private GameObject model;
+        [SerializeField] private GameObject explosionRangeShader;
+
         [SerializeField, Range(.1f, 10f)] private float explosionRadius = 1f;
         [SerializeField, Range(.1f, 10f)] private float explosionForce = 2f;
 
@@ -14,6 +18,11 @@ namespace ChainReaction
         private void Awake() => StaticActionProvider.triggerExplosion += Explode;
 
         private void OnDestroy() => StaticActionProvider.triggerExplosion -= Explode;
+
+        private void OnValidate()
+        {
+            explosionRangeShader.transform.localScale = Vector3.one * explosionRadius * 2;
+        }
 
         [ContextMenu("Explode")]
         private void Explode()
@@ -26,7 +35,7 @@ namespace ChainReaction
             {
                 if (hitCollider.TryGetComponent(out Rigidbody rigidbody))
                 {
-                    Vector3 distance = hitCollider.transform.position - transform.position;
+                    Vector3 distance = transform.position - hitCollider.transform.position;
 
                     float forceMultiplier = explosionRadius - distance.magnitude * 100f;
 
@@ -45,13 +54,8 @@ namespace ChainReaction
             GameObject vfx = Instantiate(explosionVFXPrefab, transform.position, Quaternion.identity, transform.parent);
             vfx.transform.localScale = Vector3.one * explosionRadius;
 
-            Destroy(this);
-        }
-
-        private void OnDrawGizmos()
-        {
-            Gizmos.color = Color.red;
-            Gizmos.DrawWireSphere(transform.position, explosionRadius);
+            model.SetActive(false);
+            explosionRangeShader.SetActive(false);
         }
     }
 }
