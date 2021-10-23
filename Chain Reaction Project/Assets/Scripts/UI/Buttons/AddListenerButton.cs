c8dbd72d41da6bfe5beee9c776fb501fd88ffd1d
lolesch <leonid.schreiber@yahoo.de>
2021-10-23 17:02:01 +0200

    Added more icons and UI scripts

--- Chain Reaction Project/Assets/Scripts/UI/Buttons/AddListenerButton.cs
+++ Chain Reaction Project/Assets/Scripts/UI/Buttons/AddListenerButton.cs
@@ -1,5 +1,8 @@
 using UnityEngine;
 using UnityEngine.UI;
+using DG.Tweening;
+using System.Collections.Generic;
+using UnityEngine.EventSystems;
 
 namespace ChainReaction.UI
 {
@@ -7,9 +10,18 @@ namespace ChainReaction.UI
     /// This class provides a virtual funtion that is automatically assigned to the buttons 'OnClick' event
     /// </summary>
     [RequireComponent(typeof(Button))]
-    public abstract class AddListenerButton : MonoBehaviour
+    public abstract class AddListenerButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerDownHandler, IPointerUpHandler
     {
         protected Button button;
+
+        private bool isHovering = false;
+        [SerializeField] private float animationDuration = 1f;
+
+        [Space]
+        [SerializeField] private ButtonColors colors;
+
+        private Sequence scaleUpAnimation;
+        private Sequence scaleDownAnimation;
 
         void Awake()
         {
@@ -19,5 +31,63 @@ namespace ChainReaction.UI
         }
 
         protected abstract void OnClick();
+
+        public void OnPointerEnter(PointerEventData eventData)
+        {
+            isHovering = true;
+            ScaleUpAnimation(animationDuration);
+        }
+
+        public void OnPointerExit(PointerEventData eventData)
+        {
+            isHovering = false;
+            scaleUpAnimation.Pause();
+            scaleDownAnimation.Pause();
+            ScaleDownAnimation(.2f);
+        }
+
+        public void OnPointerDown(PointerEventData eventData)
+        {
+            isHovering = false;
+
+            scaleUpAnimation.Pause();
+            scaleDownAnimation.Pause();
+
+            Sequence downAnimation = DOTween.Sequence()
+                 .Append(button.transform.DOScale(.98f, .1f).SetEase(Ease.InOutSine))
+                 .Join(button.targetGraphic.DOColor(colors.colorBlock.pressedColor, .1f).SetEase(Ease.InOutSine));
+        }
+
+        public void OnPointerUp(PointerEventData eventData) => button.transform.DOScale(1f, .1f).SetEase(Ease.InOutSine);
+
+        private void ScaleUpAnimation(float duration)
+        {
+            float from = 1f;
+            float to = 1.05f;
+            duration *= Mathf.Abs(to - transform.localScale.z) / Mathf.Abs(to - from);
+
+            scaleUpAnimation = DOTween.Sequence()
+                .Append(button.transform.DOScale(to, duration).SetEase(Ease.InOutSine))
+                .Join(button.targetGraphic.DOColor(colors.colorBlock.highlightedColor, duration).SetEase(Ease.InOutSine))
+                .OnComplete(() => { ScaleDownAnimation(animationDuration); })
+                .Pause()
+                ;
+
+            if (isHovering)
+                scaleUpAnimation.Restart();
+        }
+
+        private void ScaleDownAnimation(float duration)
+        {
+            float from = 1.05f;
+            float to = 1f;
+            duration *= Mathf.Abs(to - transform.localScale.z) / Mathf.Abs(to - from);
+
+            scaleDownAnimation = DOTween.Sequence()
+                .Append(button.transform.DOScale(to, duration).SetEase(Ease.InOutSine))
+                .Join(button.targetGraphic.DOColor(colors.colorBlock.normalColor, duration).SetEase(Ease.InOutSine))
+                .OnComplete(() => { ScaleUpAnimation(animationDuration); })
+                ;
+        }
     }
 }
