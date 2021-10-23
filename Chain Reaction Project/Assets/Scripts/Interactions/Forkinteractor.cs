12662dd164831a02a40d96b0119a052afc8fffcf
N-Banchik <87769833+N-Banchik@users.noreply.github.com>
2021-10-23 22:46:23 +0300

    Fixed Pickup for forklift for bombs and crates add coliders for explosives

--- Chain Reaction Project/Assets/Scripts/Interactions/Forkinteractor.cs
+++ Chain Reaction Project/Assets/Scripts/Interactions/Forkinteractor.cs
@@ -10,11 +10,11 @@ public class Forkinteractor : MonoBehaviour
     [SerializeField]
     private AudioSource _itemDropSound;
 
-    private HashSet<Collider> _bombNearFork = new HashSet<Collider>();
-    private Collider _heldBomb = null;
+    private HashSet<Collider> _PickableNearFork = new HashSet<Collider>();
+    private Collider _helditem = null;
 
     [SerializeField]
-    private Transform _BombPositionOffset;
+    private Transform _ItemPositionOffset;
 
     [SerializeField]
     private Transform _dropPositionOffset;
@@ -23,9 +23,9 @@ public class Forkinteractor : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag(TagsHolder.BOMB_TAG))
+        if (other.CompareTag(TagsHolder.BOMB_TAG)||other.CompareTag(TagsHolder.CRATE_TAG))
         {
-            _bombNearFork.Add(other);
+            _PickableNearFork.Add(other);
         }
 
         //TODO: add drone interaction
@@ -36,9 +36,9 @@ public class Forkinteractor : MonoBehaviour
 
     private void OnTriggerExit(Collider other)
     {
-        if (other.CompareTag(TagsHolder.BOMB_TAG))
+        if (other.CompareTag(TagsHolder.BOMB_TAG) || other.CompareTag(TagsHolder.CRATE_TAG))
         {
-            _bombNearFork.Remove(other);
+            _PickableNearFork.Remove(other);
         }
     }
 
@@ -47,23 +47,23 @@ public class Forkinteractor : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Space))
         {
             // Dropping down crate
-            if (_heldBomb != null)
+            if (_helditem != null)
             {
-                _heldBomb.transform.SetParent(null);
-                _heldBomb.transform.position = _dropPositionOffset.position;
-                _heldBomb.GetComponent<Holdables.Holdable>().DropObject();
+                _helditem.transform.SetParent(null);
+                _helditem.transform.position = _dropPositionOffset.position;
+                _helditem.GetComponent<Holdables.Holdable>().DropObject();
 
-                _heldBomb = null;
+                _helditem = null;
 
                 _itemDropSound.Play();
             }
             else
             {
                 // Picking up crate
-                if (_bombNearFork.Count > 0)
+                if (_PickableNearFork.Count > 0)
                 {
-                    Collider closestCrate = _bombNearFork.First();
-                    foreach (Collider crate in _bombNearFork)
+                    Collider closestCrate = _PickableNearFork.First();
+                    foreach (Collider crate in _PickableNearFork)
                     {
                         if (Vector3.Distance(closestCrate.transform.position, this.transform.position) >
                             Vector3.Distance(crate.transform.position, this.transform.position))
@@ -72,10 +72,10 @@ public class Forkinteractor : MonoBehaviour
                         }
                     };
                     closestCrate.transform.SetParent(this.transform);
-                    closestCrate.transform.localPosition = _BombPositionOffset.localPosition;
+                    closestCrate.transform.localPosition = _ItemPositionOffset.localPosition;
                     closestCrate.transform.localRotation = Quaternion.identity;
-                    _heldBomb = closestCrate;
-                    _heldBomb.GetComponent<Holdables.Holdable>().HoldObject();
+                    _helditem = closestCrate;
+                    _helditem.GetComponent<Holdables.Holdable>().HoldObject();
 
                     _itemPickupSound.Play();
                 }
