531746d30b4916b3415c057e8099958929273801
Paximillian <paximillian@gmail.com>
2021-10-24 01:51:44 +0200

    - Added the exit barrier script. - Added ambience.

--- Chain Reaction Project/Assets/ActivateBomb.cs
+++ Chain Reaction Project/Assets/ActivateBomb.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
+using ChainReaction;
 using UnityEngine;
 
 public class ActivateBomb : MonoBehaviour
 {
     private void OnTriggerEnter(Collider other)
     {
+        StaticActionProvider.triggerExplosion.Invoke();
         SignalBus.GameOver.Raise();    
     }
 }
