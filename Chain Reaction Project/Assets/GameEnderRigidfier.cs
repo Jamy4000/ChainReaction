47c54b9892e642802b6fe47a173ef9d8099eaaa0
Arie <arieworkmail@gmail.com>
2021-10-24 12:45:36 +0300

    Fix for shelf colliders and rigidfying at the start of the level

--- Chain Reaction Project/Assets/GameEnderRigidfier.cs
+++ Chain Reaction Project/Assets/GameEnderRigidfier.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,8 +8,15 @@ public class GameEnderRigidfier : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-       // Destroy(this.gameObject.GetComponent(typeof(Rigidbody)) as Rigidbody);
+        Rigidbody tmp = this.gameObject.AddComponent(typeof(Rigidbody)) as Rigidbody;
+        StartCoroutine(DestroyDelay());
         SignalBus.GameOver.Listen(BecomeRigid);
+    }
+
+    private IEnumerator DestroyDelay()
+    {
+        yield return new WaitForSeconds(1);
+        Destroy(this.gameObject.GetComponent(typeof(Rigidbody)) as Rigidbody);
     }
 
     void BecomeRigid()
